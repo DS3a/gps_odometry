0af1cb3d8b45aa1dfb91576ed865e940d2ea48b0
DS3a <33351122+DS3a@users.noreply.github.com>
2022-10-24 15:29:49 +0530

    fixed heading shizz

--- src/gps_odometry.rs
+++ src/gps_odometry.rs
@@ -1,24 +1,78 @@
 use utm;
 use ndarray::arr2;
+use nalgebra::Vector2;
+use std::sync::Mutex;
+
+pub struct Odometry {
+    datum_position: Vector2<f64>,
+    datum_heading: f64, // in radians
+    datum_lat: f64,
+    datum_long: f64,
+    datum_northing: f64,
+    datum_easting: f64,
+    position: Mutex<Vector2<f64>>,
+    yaw: Mutex<f64>,
+}
+
+impl Odometry {
+    pub fn new(datum_position: Vector2<f64>, datum_heading: f64, datum_lat: f64, datum_long: f64) -> Self {
+        let (datum_northing, datum_easting, _initial_meridian_convergence)
+            = utm::to_utm_wgs84_no_zone(datum_lat, datum_long);
+
+        Self {
+            datum_position,
+            datum_heading,
+            datum_lat,
+            datum_long,
+            datum_northing,
+            datum_easting,
+            position: Mutex::new(datum_position),
+            yaw: Mutex::new(datum_heading),
+        }
+    }
+
+    pub fn transform_utm_to_local_frames(&self, relative_northing: f64, relative_easting: f64) -> (f64, f64) {
+        /*
+         * returns (x, y)
+         * X-axis => North axis
+         * Y-axis => East axis
+         */
+
+        (0f64, 0f64)
+
+    }
+
+    pub fn update_odom(&self, lat: f64, long: f64) {
+        let (northing, easting, _meridian_convergence) = utm::to_utm_wgs84_no_zone(lat, long);
+    }
+}
 
 /*
 TODO decide if magnetometer can be used to get stuff done or if odometry has to be used
 */
-
 pub fn get_heading(mag_x: f64, mag_y: f64) -> f64 {
     // angle with respect to north
     // CW = +ve ??
     // ACW = +ve
-    mag_y.atan2(mag_x) * 180.0 / std::f64::consts::PI
+    // N = 0 degrees
+    // W = 90 degrees
+    // S = 180 degrees
+    // E = -90 degrees
+
+    // TODO add magnetic declination based on current lat long etc.
+    // output in radians
+    -mag_y.atan2(mag_x)
 }
 
 
 // Easting = eastward measured distance
 // Northing = nortward measured distance
 //
-pub fn map_to_utm_tf(map_x: f64, map_y: f64, utm_x: f64, utm_y: f64) -> f64 {
+pub fn map_to_utm_tf(map_x: f64, map_y: f64, utm_n: f64, utm_e: f64) -> f64 {
     0.0f64
 }
+
+// TODO function to get the x and y in map frame given northing and easting
 
 
 // NO clue how to proceed
--- src/gps_odometry_node.rs
+++ src/gps_odometry_node.rs
@@ -12,18 +12,19 @@ use std::thread;
 fn main() -> Result<(), Error> {
     let context = rclrs::Context::new(env::args())?;
 
-
     let gnss_msg_global_handler = Arc::new(Mutex::new(NavSatFixMsg::default()));
     let imu_msg_global_handler = Arc::new(Mutex::new(ImuMsg::default()));
+    let odom_msg_global_handler = Arc::new(Mutex::new(OdometryMsg::default()));
 
     let mut node = rclrs::create_node(&context, "gps_odometry_node").unwrap();
-
-    let publisher = node.create_publisher::<std_msgs::msg::String>("topic", rclrs::QOS_PROFILE_DEFAULT).unwrap();
 
     let odom_publisher = node.create_publisher::<OdometryMsg>("/odometry/gps", rclrs::QOS_PROFILE_DEFAULT).unwrap();
 
     let gnss_msg_subscriber_handler = Arc::clone(&gnss_msg_global_handler);
     let _gnss_subscriber = node.create_subscription::<NavSatFixMsg,  _>("/gnss", rclrs::QOS_PROFILE_DEFAULT, move |msg: NavSatFixMsg| {
+        // TODO measure the frequency of the GPS signal
+        let time = &msg.header.stamp;
+        println!("{:?}", time);
         *gnss_msg_subscriber_handler.lock().unwrap() = msg;
     });
 
@@ -32,19 +33,14 @@ fn main() -> Result<(), Error> {
         *imu_msg_subscriber_handler.lock().unwrap() = msg;
     });
 
-    let mut message = std_msgs::msg::String::default();
-
-    let mut publish_count: u32 = 1;
-
     thread::spawn(move || {
         rclrs::spin(&node).unwrap();
     });
 
     while context.ok() {
-        message.data = format!("Hello, world! {}", publish_count);
-        println!("Publishing: [{}]", message.data);
-        publisher.publish(&message);
-        publish_count += 1;
+        odom_publisher.publish(&*odom_msg_global_handler.lock().unwrap());
+        println!("{:?}", &odom_msg_global_handler.lock().unwrap().header.stamp.sec);
+        // TODO replace this with adaptive rate inversely proportional to gps_signal frequency
         std::thread::sleep(std::time::Duration::from_millis(500));
     }
     Ok(())
