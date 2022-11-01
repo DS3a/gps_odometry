46c12a19d49efc17e222385a7709aa0d293ca57f
DS3a <33351122+DS3a@users.noreply.github.com>
2022-11-01 16:19:01 +0530

    cleaned code

--- src/gps_odometry.rs
+++ src/gps_odometry.rs
@@ -1,7 +1,6 @@
-use utm;
-use ndarray::arr2;
-use nalgebra::{Vector2, Matrix2};
+use nalgebra::{Matrix2, Vector2};
 use std::sync::Mutex;
+use utm;
 
 // done TODO verify axes
 // TODO add magnetic declination somewhere
@@ -19,8 +18,8 @@ pub struct Odometry {
 
 impl Odometry {
     pub fn new(datum_heading: f64, datum_lat: f64, datum_long: f64) -> Self {
-        let (datum_northing, datum_easting, _initial_meridian_convergence)
-            = utm::to_utm_wgs84_no_zone(datum_lat, datum_long);
+        let (datum_northing, datum_easting, _initial_meridian_convergence) =
+            utm::to_utm_wgs84_no_zone(datum_lat, datum_long);
 
         Self {
             // datum_position, // is this required?
@@ -34,7 +33,11 @@ impl Odometry {
         }
     }
 
-    pub fn transform_utm_to_local_frames(&self, relative_northing: f64, relative_easting: f64) -> Vector2<f64> {
+    pub fn transform_utm_to_local_frames(
+        &self,
+        relative_northing: f64,
+        relative_easting: f64,
+    ) -> Vector2<f64> {
         /*
          * returns (x, y)
          * X-axis => North axis
@@ -44,7 +47,9 @@ impl Odometry {
         let heading = -1f64 * self.datum_heading;
         let cosine = heading.cos();
         let sine = heading.sin();
-        Matrix2::new(-1f64, 0f64, 0f64, 1f64) * Matrix2::new(cosine, -sine, sine, cosine) * Vector2::new(relative_northing, relative_easting)
+        Matrix2::new(-1f64, 0f64, 0f64, 1f64)
+            * Matrix2::new(cosine, -sine, sine, cosine)
+            * Vector2::new(relative_northing, relative_easting)
     }
     // c, s, -s, c gives (-22, 13)
 
@@ -56,7 +61,6 @@ impl Odometry {
         let translation = self.transform_utm_to_local_frames(relative_northing, relative_easting);
         *self.position.lock().unwrap() = translation;
     }
-
 }
 
 pub fn get_heading(mag_x: f64, mag_y: f64) -> f64 {
@@ -68,7 +72,6 @@ pub fn get_heading(mag_x: f64, mag_y: f64) -> f64 {
     // S = 180 degrees
     // E = -90 degrees
 
-
     // TODO add magnetic declination based on current lat long etc.
     // output in radians
     let heading = -mag_y.atan2(mag_x);
@@ -78,11 +81,9 @@ pub fn get_heading(mag_x: f64, mag_y: f64) -> f64 {
     return heading;
 }
 
-
 // Easting = eastward measured distance
 // Northing = nortward measured distance
 //
 pub fn map_to_utm_tf(map_x: f64, map_y: f64, utm_n: f64, utm_e: f64) -> f64 {
     0.0f64
 }
-
--- src/gps_odometry_node.rs
+++ src/gps_odometry_node.rs
@@ -1,16 +1,14 @@
 use anyhow::{Error, Result};
-use std::env;
 use gps_odometry;
-use rclrs;
 use nav_msgs::msg::Odometry as OdometryMsg;
-use sensor_msgs::msg::NavSatFix as NavSatFixMsg;
-use sensor_msgs::msg::Imu as ImuMsg;
+use rclrs;
 use sensor_msgs::msg::MagneticField as MagneticFieldMsg;
-use std::sync::{Arc, Mutex};
+use sensor_msgs::msg::NavSatFix as NavSatFixMsg;
+use std::env;
 use std::sync::mpsc;
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{Arc, Mutex};
 use std::thread;
-
 
 fn main() -> Result<(), Error> {
     let context = rclrs::Context::new(env::args())?;
@@ -19,31 +17,38 @@ fn main() -> Result<(), Error> {
     let mag_msg_global_handler = Arc::new(Mutex::new(Option::<MagneticFieldMsg>::None));
     let odom_msg_global_handler = Arc::new(Mutex::new(Some(OdometryMsg::default())));
 
-
     // this channel will be used to wake up the thread that will be used to unfreeze the odom publisher thread whenever a gps ping is recieved
     let (gps_msg_tx, gps_msg_rx): (Sender<bool>, Receiver<bool>) = mpsc::channel();
     let mut node = rclrs::create_node(&context, "gps_odometry_node").unwrap();
 
-    let odom_publisher = node.create_publisher::<OdometryMsg>("/odometry/gps", rclrs::QOS_PROFILE_DEFAULT).unwrap();
+    let odom_publisher = node
+        .create_publisher::<OdometryMsg>("/odometry/gps", rclrs::QOS_PROFILE_DEFAULT)
+        .unwrap();
 
     let gnss_msg_subscriber_handler = Arc::clone(&gnss_msg_global_handler);
-    let gps_msg_subscriber_tx = gps_msg_tx.clone();
-    let _gnss_subscriber = node.create_subscription::<NavSatFixMsg,  _>("/gnss", rclrs::QOS_PROFILE_DEFAULT, move |msg: NavSatFixMsg| {
-        *gnss_msg_subscriber_handler.lock().unwrap() = Some(msg);
-        gps_msg_tx.send(true).unwrap();
-    });
+    let _gnss_subscriber = node.create_subscription::<NavSatFixMsg, _>(
+        "/gnss",
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: NavSatFixMsg| {
+            *gnss_msg_subscriber_handler.lock().unwrap() = Some(msg);
+            gps_msg_tx.send(true).unwrap();
+        },
+    );
 
     let mag_msg_subscriber_handler = Arc::clone(&mag_msg_global_handler);
-    let _mag_subscriber = node.create_subscription::<MagneticFieldMsg, _>("/imu/mag", rclrs::QOS_PROFILE_DEFAULT, move |msg: MagneticFieldMsg| {
-        *mag_msg_subscriber_handler.lock().unwrap() = Some(msg);
-    });
+    let _mag_subscriber = node.create_subscription::<MagneticFieldMsg, _>(
+        "/imu/mag",
+        rclrs::QOS_PROFILE_DEFAULT,
+        move |msg: MagneticFieldMsg| {
+            *mag_msg_subscriber_handler.lock().unwrap() = Some(msg);
+        },
+    );
 
     thread::spawn(move || {
         rclrs::spin(&node).unwrap();
     });
 
-    let mut gps_odometry_opt = Option::<gps_odometry::Odometry>::None;
-
+    let gps_odometry_opt: Option::<gps_odometry::Odometry>;
     loop {
         if let Some(gnss_msg) = &*gnss_msg_global_handler.lock().unwrap() {
             println!("got datum lat long");
@@ -51,8 +56,8 @@ fn main() -> Result<(), Error> {
                 println!("got datum yaw");
                 // done TODO get yaw heading from `gps_odometry::get_heading`
                 // done
-                let datum_heading = gps_odometry::get_heading(mag_msg.magnetic_field.x,
-                                                              mag_msg.magnetic_field.y);
+                let datum_heading =
+                    gps_odometry::get_heading(mag_msg.magnetic_field.x, mag_msg.magnetic_field.y);
                 let (lat, long) = (gnss_msg.latitude, gnss_msg.longitude);
                 // gps_odometry_opt = Option::<gps_odometry::Odometry>::None;
                 // done TODO replace this       ^^^^ with the init method
@@ -77,19 +82,18 @@ fn main() -> Result<(), Error> {
                 println!("got gnss msg, now waiting for odom instance");
                 odom_msg.header.stamp.sec = gnss_msg.header.stamp.sec;
                 odom_msg.header.stamp.nanosec = gnss_msg.header.stamp.nanosec;
-               if let Some(gps_odometry_instance) = &gps_odometry_opt {
-                   // TODO call function that modifies odom based on the gnss msg
-                   // done
-                   gps_odometry_instance.update_odom(gnss_msg.latitude, gnss_msg.longitude);
-                   // TODO update odom_msg based on what is given out by gps_odometry_opt
-                   // done
-                   let position = *gps_odometry_instance.position.lock().unwrap();
-                   odom_msg.pose.pose.position.x = position.x;
-                   odom_msg.pose.pose.position.y = position.y;
-                   println!("sending odom");
-                   println!("Current position: {}, {}", position.x, position.y);
-                   odom_publisher.publish(&*odom_msg).unwrap();
-               }
+                if let Some(gps_odometry_instance) = &gps_odometry_opt {
+                    // TODO call function that modifies odom based on the gnss msg
+                    // done
+                    gps_odometry_instance.update_odom(gnss_msg.latitude, gnss_msg.longitude);
+                    // TODO update odom_msg based on what is given out by gps_odometry_opt
+                    // done
+                    let position = *gps_odometry_instance.position.lock().unwrap();
+                    odom_msg.pose.pose.position.x = position.x;
+                    odom_msg.pose.pose.position.y = position.y;
+                    println!("Current position: {}, {}", position.x, position.y);
+                    odom_publisher.publish(&*odom_msg).unwrap();
+                }
             }
         }
     }
